465f15374c3c229b87e49202014304e80bd0e534
Yiğit Levent <yigitlevent@live.com>
2020-12-02 19:49:32 +0300

    small fix to the 0.5.3 version
    Check CHANGELOG.md

--- server/src/shared/bot.ts
+++ server/src/shared/bot.ts
@@ -5,11 +5,11 @@ import { discordClient } from "../bin/www";
 export function sendRollResult(guildID: string, channelName: string, message: string, username: string): void {
 	const guild = discordClient.guilds.cache.get(guildID);
 	if (guild) {
-		const member = guild.members.cache.find((member: GuildMember) => { return member.displayName === username; });
+		const member = guild.members.cache.find((member: GuildMember) => { return member.nickname === username; });
 		const channel = guild.channels.cache.find((channel: Channel) => { return (channel as TextChannel).name === channelName; });
 
 		if (channel && channel.type === "text") {
-			(channel as TextChannel).send(`${(member) ? `<@${member.user.id}> ` : ""}${message.charAt(0).toLowerCase() + message.slice(1)}`);
+			(channel as TextChannel).send(`${(member) ? `<@${member.user.id}> ` : ""}${(member) ? message.charAt(0).toLowerCase() + message.slice(1) : message}`);
 		}
 	}
 }
