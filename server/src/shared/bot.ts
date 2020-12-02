d98ef3b14fbc70485cab86ea2af01180b455955c
Yiğit Levent <yigitlevent@live.com>
2020-12-02 20:12:31 +0300

    okay, last test

--- server/src/shared/bot.ts
+++ server/src/shared/bot.ts
@@ -6,15 +6,17 @@ export function sendRollResult(guildID: string, channelName: string, message: st
 	const guild = discordClient.guilds.cache.get(guildID);
 	if (guild) {
 
+		guild.members.fetch()
+			.then((value) => {
+				const member = value.find((member: GuildMember) => { console.log(member.nickname); console.log(member.displayName); return member.nickname === username; });
 
-		console.log(guild.members.cache);
-		const member = guild.members.cache.find((member: GuildMember) => { console.log(member); return member.nickname === username; });
+				console.log(member);
 
-		console.log(member);
-		const channel = guild.channels.cache.find((channel: Channel) => { return (channel as TextChannel).name === channelName; });
+				const channel = guild.channels.cache.find((channel: Channel) => { return (channel as TextChannel).name === channelName; });
 
-		if (channel && channel.type === "text") {
-			(channel as TextChannel).send(`${(member) ? `<@${member.user.id}> ` : ""}${(member) ? message.charAt(0).toLowerCase() + message.slice(1) : message}`);
-		}
+				if (channel && channel.type === "text") {
+					(channel as TextChannel).send(`${(member) ? `<@${member.user.id}> ` : ""}${(member) ? message.charAt(0).toLowerCase() + message.slice(1) : message}`);
+				}
+			});
 	}
 }
